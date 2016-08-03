c2f4c50a84b8b3b2d139b56aa631bd11279b9c52
Ivar Abrahamsen <ivar@flurdy.com>
2016-08-03 22:26:31 +0100

    Collapsable tags and characteristics

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -16,7 +16,7 @@ trait WithNewsBar {
 
    def projectLookup: ProjectLookup
 
-   implicit def latestNews: List[News] = projectLookup.findNews(15)
+   implicit def latestNews: List[News] = projectLookup.findNews(18)
 }
 
 @Singleton
