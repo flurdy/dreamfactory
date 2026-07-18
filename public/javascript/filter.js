86dadb7dd9de93bad85693378b05873f168e6e19
Ivar Abrahamsen <ivarabrahamsen@bluelightcard.co.uk>
2026-07-18 18:26:33 +0100

    feat: modernize project discovery and filters

--- public/javascript/filter.js
+++ public/javascript/filter.js
@@ -1,20 +1,17 @@
-$('#nautical-cargo .properties-filter label').on('mouseup', function (e) {
-   var radioId = $(this).attr('for');
-   var radio = $("#" + radioId);
-   if (radio.is(':checked')) {
-      radio.prop('checked', false);
-   } else {
-      radio.prop('checked', true);
-   }
-   $('#nautical-cargo #filter-accordion form').submit();
-});
-
-// Disable default behavior
-$('#nautical-cargo .properties-filter label').click(function (e) {
-   e.preventDefault();
+document.querySelectorAll('.properties-filter-form').forEach(function (form) {
+   form.addEventListener('submit', function () {
+      form.querySelectorAll('.property-filter-any:checked').forEach(function (filter) {
+         filter.disabled = true;
+      });
+   });
 });
 
 function resetFilter(form) {
-   $('#nautical-cargo .properties-filter input[type="radio"]').attr('checked', false);
-   $('#nautical-cargo #filter-accordion form').submit();
+   var filters = form.querySelectorAll('[name^="filter."]');
+
+   filters.forEach(function (filter) {
+      filter.disabled = true;
+   });
+
+   form.submit();
 }
